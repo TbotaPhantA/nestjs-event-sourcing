fce37259c68f2a16f92a8dedff3f36a89a9a2b56
TbotaPhantA <botakiller@mail.ru>
2023-11-14 22:11:32 +0000

    minor fix.

--- src/sales/application/salesProduct/salesProduct.controller.ts
+++ src/sales/application/salesProduct/salesProduct.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CreateSalesProduct } from '../../domain/salesProduct/commands/createSalesProduct';
 import { CreateSalesProductService } from './services/createSalesProduct.service';
+import { CreateSalesProductOutputDto } from './dto/createSalesProduct.output.dto';
 
 @Controller('sales/product')
 export class SalesProductController {
@@ -10,7 +11,9 @@ export class SalesProductController {
 
   @Post()
   @HttpCode(HttpStatus.NO_CONTENT)
-  async createSalesProduct(@Body() command: CreateSalesProduct): Promise<void> {
-    await this.createSalesProductService.create(command);
+  async createSalesProduct(
+    @Body() command: CreateSalesProduct,
+  ): Promise<CreateSalesProductOutputDto> {
+    return this.createSalesProductService.create(command);
   }
 }
