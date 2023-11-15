60f5c3432d10360cb9f9b8353af9492e3a847a9e
TbotaPhantA <botakiller@mail.ru>
2023-11-15 19:23:14 +0000

    `random.module.ts` was moved.

--- src/sales/infrastructure/random/random.module.ts
+++ src/sales/infrastructure/random/random.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { RandomService } from './random/random.service';
+import { RandomService } from './random.service';
 
 @Module({
   providers: [RandomService],
