5847b417f36d93535526130813037f0800465b43
TbotaPhantA <botakiller@mail.ru>
2023-11-15 21:12:32 +0000

    AdjustPriceService was added.

--- src/sales/application/sales.module.ts
+++ src/sales/application/sales.module.ts
@@ -5,12 +5,14 @@ import { SALES_PRODUCT_REPOSITORY } from './salesProduct/shared/constants';
 import { TransactionModule } from '../infrastructure/transaction/transaction.module';
 import { InMemorySalesProductRepository } from './salesProduct/repositories/salesProduct.repository.ts/InMemorySalesProduct.repository';
 import { SalesProductController } from './salesProduct/salesProduct.controller';
+import { AdjustPriceService } from './salesProduct/services/adjustPrice.service';
 
 @Module({
   imports: [RandomModule, TransactionModule],
   controllers: [SalesProductController],
   providers: [
     CreateSalesProductService,
+    AdjustPriceService,
     {
       provide: SALES_PRODUCT_REPOSITORY,
       useClass: InMemorySalesProductRepository,
