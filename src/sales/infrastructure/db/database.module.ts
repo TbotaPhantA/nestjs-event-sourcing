214404d7dea384998252880a9df053943f72bec7
TbotaPhantA <botakiller@mail.ru>
2023-11-16 22:44:24 +0000

    `typeorm` packages were removed, because ORM is not needed with event sourcing.

--- src/sales/infrastructure/db/database.module.ts
+++ src/sales/infrastructure/db/database.module.ts
@@ -1,19 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from '../config/config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: config.db.host,
-      port: config.db.port,
-      username: config.db.username,
-      password: config.db.password,
-      database: config.db.database,
-      entities: [],
-      synchronize: config.db.synchronize,
-    }),
-  ],
+  imports: [],
 })
 export class DatabaseModule {}
