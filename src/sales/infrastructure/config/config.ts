741c609dadb4909e417bfbb9fc3c9dbc8e5007f2
TbotaPhantA <botakiller@mail.ru>
2023-11-16 19:32:57 +0000

    config infrastructure module was added.

---
+++ src/sales/infrastructure/config/config.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import * as process from 'process';
+
+dotenv.config();
+
+const isRequired = (propName: keyof NodeJS.ProcessEnv): never => {
+  throw new Error(`Config property ${propName} is required`);
+};
+
+class Config {
+  app = {
+    port: process.env.APP_PORT ?? isRequired('APP_PORT'),
+  };
+
+  db = {
+    host: process.env.DB_HOST ?? isRequired('DB_HOST'),
+    port: process.env.DB_PORT ?? isRequired('DB_PORT'),
+    username: process.env.DB_USERNAME ?? isRequired('DB_USERNAME'),
+    password: process.env.DB_PASSWORD ?? isRequired('DB_PASSWORD'),
+    database: process.env.DB_DATABASE ?? isRequired('DB_DATABASE'),
+    synchronize: process.env.DB_SYNCHRONIZE ?? isRequired('DB_SYNCHRONIZE'),
+  };
+}
+
+export const config = new Config();
